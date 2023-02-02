f26b7ff7d1726c3e0ee4de8879af90c5b087c86c
jerikorir@gmail.com <jerikorir@gmail.com>
2023-02-02 16:55:37 +0300

    Add project files.

---
+++ ProductsApi/Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using ProductsApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductsApi.Controllers
+{
+    public class ProductController : ApiController
+    {
+        Entities db = new Entities();
+
+        //Add product Post
+        public string Post(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return "Item added sucessfully";
+        }
+        //Get all products from table
+        public IEnumerable<Product> GetAll()
+        {
+            return db.Products.ToList();
+        }
+
+        //Get product by Id
+        public Product GetById(int id)
+        {
+            return db.Products.Find(id);
+        }
+
+        //Update the product
+        public string Put(int id, Product product)
+        {
+            var oproduct = db.Products.Find(id);
+            if (oproduct != null)
+            {
+                oproduct.Name = product.Name;
+                oproduct.Price = product.Price;
+                oproduct.Quantity = product.Quantity;
+                oproduct.Active = product.Active;
+
+                db.Entry(oproduct).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+            return "Item has been updated sucessfully";
+        }
+
+        //delete product
+        public string Delete(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (product != null)
+            {
+                db.Products.Remove(product);
+                db.SaveChanges();
+            }
+            return "Item deleted sucessfully";
+        }
+    }
+}
---
+++ ProductsApi/Scripts/Product.js
@@ -0,0 +1,101 @@
+﻿$(document).ready(function () {
+    getProducts();
+});
+//add or post product
+function saveProduct() {
+    var url = "/api/Product";
+    var objectProduct = {};
+
+    objectProduct.Name = $('#txtProductName').val();
+    objectProduct.Price = $('#txtPrice').val();
+    objectProduct.Quantity = $('#txtQuantity').val();
+    var btnAct = $('#btnActive').is(':checked');
+    if (btnAct) {
+        objectProduct.Active = 1;
+    }
+    else {
+        objectProduct.Active = 0
+    }
+
+    if (objectProduct) {
+        $.ajax({
+            url: url,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            data: JSON.stringify(objectProduct),
+            type: "Post",
+            success: function (result) {
+                clearFields();
+                //alert(result);
+                getProducts()
+            },
+            error: function (msg) {
+                alert(msg);
+            }
+        });
+    }
+}
+
+
+//clear fields function
+function clearFields() {
+    $('#txtProductName').val('');
+    $('#txtPrice').val('');
+    $('#txtQuantity').val('');
+}
+
+//get all products
+function getProducts() {
+    var url = "/api/Product";
+    $.ajax({
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        type: "Get",
+        success: function (result) {
+            //alert(result);
+            //alert(JSON.stringify(result));
+
+            if (result) {
+                $('#tbodyProduct').html(''); //to only append once from database
+                var row = '';
+                for(let i=0; i < result.length; i++){
+           row = row
+            + "<tr>"
+            + "<td>" + result[i].Name + "</td>"
+            + "<td>" + result[i].Price + "</td>"
+            + "<td>" + result[i].Quantity + "</td>"
+               + "<td>" + result[i].Active + "</td>"
+               + "<td><button class='btn btn-success' onclick='deleteProduct(" + result[i].Id + ")'>Edit</button></td>"
+               + "<td><button class='btn btn-danger' onclick='deleteProduct(" + result[i].Id + ")'>Delete</button></td>"
+            + "</tr>";
+                }
+        if (row != '') {
+        $('#tbodyProduct').append(row);
+            }
+            }
+        },
+        error: function (msg) {
+            alert(msg);
+        }
+    });
+}
+
+//delete product
+function deleteProduct(id) {
+    var url = "/api/Product/" + id;
+        $.ajax({
+            url: url,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            type: "Delete",
+            success: function (result) {
+                clearFields();
+                alert(result);
+                getProducts()
+            },
+            error: function (msg) {
+                alert(msg);
+            }
+        });
+    }
